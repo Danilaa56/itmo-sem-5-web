5ad2d2f26c06ce8e45fdcaf3c11cc791f55c1daa
Danilaa56 <danila.aronov@gmail.com>
2022-12-13 17:02:27 +0300

    feat: cache songs data from the first request feat: add api search method feat: add search page

--- src/staff/api.js
+++ src/staff/api.js
@@ -1,26 +1,59 @@
-let api = {
-    songs: {
-        list: () => {
-            return fetch(`data/songs.json`)
-                .then(response => {
-                    if (response.status === 200) {
-                        return response.json();
-                    } else {
-                        alert("Failed to list songs: " + response.statusText);
-                    }
-                });
-        },
-        byId: (id) => {
-            return api.songs.list()
-                .then(songs => {
+let api = (() => {
+    let songsJson = undefined;
+    return {
+        songs: {
+            list: () => {
+                if (songsJson !== undefined) {
+                    return Promise.resolve(Array.from(songsJson));
+                }
+                return fetch(`data/songs.json`)
+                    .then(async response => {
+                        if (response.status === 200) {
+                            let songsJson = await response.json();
+                            return Promise.resolve(Array.from(songsJson));
+                        } else {
+                            alert("Failed to list songs: " + response.statusText);
+                        }
+                    });
+            },
+            byId: (id) => {
+                return api.songs.list()
+                    .then(songs => {
+                        for (let song of songs) {
+                            // noinspection EqualityComparisonWithCoercionJS
+                            if (song.id == id) {
+                                return song;
+                            }
+                        }
+                        alert(`Failed to find song with id '${id}'`);
+                    })
+            },
+            search: (query) => {
+                function stringToWords(str) {
+                    return new Set(Array.from(str.toLocaleLowerCase().matchAll('[а-яёa-z]+')).map(a => a[0]));
+                }
+
+                let listPromise = api.songs.list();
+                let queryWords = stringToWords(query);
+
+                return listPromise.then(songs => {
+                    let result = [];
                     for (let song of songs) {
-                        // noinspection EqualityComparisonWithCoercionJS
-                        if (song.id == id) {
-                            return song;
+                        let songWords = stringToWords(song.lyrics);
+                        let isValid = true;
+                        for (let word of queryWords) {
+                            if (!songWords.has(word) && song.fullTitle.indexOf(word) === -1) {
+                                isValid = false;
+                                break;
+                            }
+                        }
+                        if (isValid) {
+                            result.push(song);
                         }
                     }
-                    alert(`Failed to find song with id '${id}'`);
+                    return result;
                 })
-        }
-    },
-}
+            }
+        },
+    }
+})();
